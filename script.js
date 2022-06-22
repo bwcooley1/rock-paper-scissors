451fe252ea6765841aa61b7597c17f6d6f746090
Brandon Cooley <bwcooley1@icloud.com>
2022-06-21 23:24:08 -0500

    added for loop and win/lose

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-let array = ["rock","paper","scissors"]
-const computerPlay =(array[Math.floor(Math.random() * array.length)])
-console.log(computerPlay);
-
-const playerSelection=prompt ("Rock, Paper, or Scissors?");
-const computerSelection=computerPlay;
+let computerScore=0;
+let playerScore=0;
 
 
 function playRound (playerSelection, computerSelection) {
     
 
     if (playerSelection.toLowerCase()==="rock" && computerSelection==="paper") {
-        return("You lose! Paper beats rock.")
+        return("You lose! Paper beats rock."+ "The score is "+ playerScore+" to "+(computerScore++));
+        
     }
     if (playerSelection.toLowerCase()==="rock" && computerSelection==="scissors") {
-        return("You win! Rock beats scissors!")
+        return("You win! Rock beats scissors!"+ "The score is "+ (playerScore++)+" to "+computerScore);
+       
     }
     if (playerSelection.toLowerCase()==="paper" && computerSelection==="scissors") {
-        return("You lose! Scissors beats paper.")
+        return("You lose! Scissors beats paper."+ "The score is "+ playerScore+" to "+(computerScore++))
     }
     if (playerSelection.toLowerCase()==="paper" && computerSelection==="rock") {
-        return("You win! Paper beats rock!")
+        return("You win! Paper beats rock!"+ "The score is "+ (playerScore++)+" to "+computerScore)
     }
     if (playerSelection.toLowerCase()==="scissors" && computerSelection==="rock") {
-        return("You lose! Rock beats scissors.")
+        return("You lose! Rock beats scissors."+ "The score is "+ playerScore+" to "+(computerScore++))
     }
     if (playerSelection.toLowerCase()==="scissors" && computerSelection==="paper") {
-        return("You win! Scissors beats paper!")
+        return("You win! Scissors beats paper!"+ "The score is "+ (playerScore++)+" to "+computerScore)
     }
     if (playerSelection.toLowerCase() === computerSelection) { 
-        return("It's a Tie! Play again!")
+        return("It's a Tie! Play again!"+"The score is "+playerScore+" to "+computerScore)
     }
 }
 
 
-console.log(playRound(playerSelection,computerSelection));
-
-//function game () {
-//for (let i = 0; i < 5; i++) {
-    // your code here!
-// }
-//}
+  for (let i = 0; i < 5; i++) {
+  let array = ["rock","paper","scissors"]
+  const computerPlay =(array[Math.floor(Math.random() * array.length)]);
+  console.log(computerPlay);
+  let playerSelection=prompt ("Rock, Paper, or Scissors?");
+  let computerSelection=computerPlay;
+  console.log(playRound(playerSelection, computerSelection))
+ 
+  if (playerScore ==5) {
+    console.log("You beat the computer!")
+  }
+  if (computerScore ==5) {
+    console.log("The computer smoked you!")
+  }
+  }
