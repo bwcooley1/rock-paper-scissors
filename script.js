eed64877a074d1fe8ae14073782d2147ff80fecc
Brandon Cooley <bwcooley1@icloud.com>
2022-06-21 21:53:21 -0500

    corrected if/else statements

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 let array = ["rock","paper","scissors"]
-const computerPlay = array[Math.floor(Math.random() * array.length)];
+const computerPlay =(array[Math.floor(Math.random() * array.length)])
 console.log(computerPlay);
 
-
+const playerSelection=prompt ("Rock, Paper, or Scissors?");
+const computerSelection=computerPlay;
 
 
 function playRound (playerSelection, computerSelection) {
     
 
-    if (playerSelection.toLowerCase()==="rock",computerSelection==="paper") {
+    if (playerSelection.toLowerCase()==="rock" && computerSelection==="paper") {
         return("You lose! Paper beats rock.")
     }
-    else if (playerSelection.toLowerCase()==="rock",computerSelection==="scissors") {
+    if (playerSelection.toLowerCase()==="rock" && computerSelection==="scissors") {
         return("You win! Rock beats scissors!")
     }
-    else if (playerSelection.toLowerCase()==="paper",computerSelection==="scissors") {
+    if (playerSelection.toLowerCase()==="paper" && computerSelection==="scissors") {
         return("You lose! Scissors beats paper.")
     }
-    else if (playerSelection.toLowerCase()==="paper",computerSelection==="rock") {
+    if (playerSelection.toLowerCase()==="paper" && computerSelection==="rock") {
         return("You win! Paper beats rock!")
     }
-    else if (playerSelection.toLowerCase()==="scissors",computerSelection==="rock") {
+    if (playerSelection.toLowerCase()==="scissors" && computerSelection==="rock") {
         return("You lose! Rock beats scissors.")
     }
-    else if (playerSelection.toLowerCase()==="scissors",computerSelection==="paper") {
+    if (playerSelection.toLowerCase()==="scissors" && computerSelection==="paper") {
         return("You win! Scissors beats paper!")
     }
-    else if (playerSelection.toLowerCase() === computerSelection) { 
+    if (playerSelection.toLowerCase() === computerSelection) { 
         return("It's a Tie! Play again!")
     }
 }
 
-const playerSelection=prompt ("Rock, Paper, or Scissors?");
-const computerSelection=computerPlay;
-console.log(playRound(playerSelection, computerSelection));
+
+console.log(playRound(playerSelection,computerSelection));
+
+//function game () {
+//for (let i = 0; i < 5; i++) {
+    // your code here!
+// }
+//}
