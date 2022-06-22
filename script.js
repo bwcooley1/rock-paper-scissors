939b3ead8a23c2d92753b9fd40bf0d7f8c30725d
Brandon Cooley <bwcooley1@icloud.com>
2022-06-21 21:18:23 -0500

    added if/else statements and player input prompt

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-computerPlay
+let array = ["rock","paper","scissors"]
+const computerPlay = array[Math.floor(Math.random() * array.length)];
+console.log(computerPlay);
+
+
+
+
+function playRound (playerSelection, computerSelection) {
+    
+
+    if (playerSelection.toLowerCase()==="rock",computerSelection==="paper") {
+        return("You lose! Paper beats rock.")
+    }
+    else if (playerSelection.toLowerCase()==="rock",computerSelection==="scissors") {
+        return("You win! Rock beats scissors!")
+    }
+    else if (playerSelection.toLowerCase()==="paper",computerSelection==="scissors") {
+        return("You lose! Scissors beats paper.")
+    }
+    else if (playerSelection.toLowerCase()==="paper",computerSelection==="rock") {
+        return("You win! Paper beats rock!")
+    }
+    else if (playerSelection.toLowerCase()==="scissors",computerSelection==="rock") {
+        return("You lose! Rock beats scissors.")
+    }
+    else if (playerSelection.toLowerCase()==="scissors",computerSelection==="paper") {
+        return("You win! Scissors beats paper!")
+    }
+    else if (playerSelection.toLowerCase() === computerSelection) { 
+        return("It's a Tie! Play again!")
+    }
+}
+
+const playerSelection=prompt ("Rock, Paper, or Scissors?");
+const computerSelection=computerPlay;
+console.log(playRound(playerSelection, computerSelection));
